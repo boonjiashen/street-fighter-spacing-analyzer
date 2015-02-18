496571eb81fd348fa7a621fb0fcf8cedd0bd2ec0
Jia-Shen Boon <jiashen@gmail.com>
2015-02-17 23:11:48 -0600

    Write example of OpenCV videowriter

---
+++ opencv_examples/example_videowriter.py
@@ -0,0 +1,29 @@
+#Example script of writing a video
+
+import cv2
+import numpy as np
+import itertools
+
+if __name__ == "__main__":
+
+    filename = 'output.avi'  # filename of video to be saved
+
+    # Intensities go from 0 to 255 and back down to 0 and repeat
+    intensities = range(0, 256, 10)
+    intensities = itertools.cycle(
+            itertools.chain(intensities, reversed(intensities)))
+
+    # Define the codec and create VideoWriter object
+    codec = cv2.VideoWriter_fourcc(*'MJPG')
+    fps = 30.
+    sz = (200, 200)  # frame size
+    out = cv2.VideoWriter(filename, codec, fps, sz)
+
+    n_frames = 1000
+    for intensity in itertools.islice(intensities, n_frames):
+        image = intensity * np.ones(list(sz) + [3], dtype=np.uint8)
+        out.write(image)
+
+    out.release()
+
+    print('Saved video', filename)
