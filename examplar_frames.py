aeb3ca83f63de88541fc8a9ee0e2dd3ae4bf4903
jiashen <jiashen@jiashens-MacBook-Air.local>
2015-11-27 11:58:59 -0600

    Make more OOP

--- examplar_frames.py
+++ examplar_frames.py
@@ -17,6 +17,85 @@ import itertools
 import sklearn.cluster
 import sklearn.pipeline
 
+class FrameExemplifier():
+
+    def __init__(self, num_examplars = 25):
+        self.num_examplars = num_examplars
+        kmeans = (sklearn.cluster.KMeans,
+                {
+                    'n_clusters': self.num_examplars,
+                    #'n_jobs': -1,
+                    'n_init': 10,
+                    'max_iter': 10,
+                })
+
+        steps = [kmeans]
+        self.pipeline = sklearn.pipeline.make_pipeline(
+                *[fun(**kwargs) for fun, kwargs in steps])
+
+        util.print_steps(steps)
+
+    def get_from_features(self, X):
+        """Returns the indices of the examplar of dataset X
+
+        Returns a n_examplars length array where each element is in [0, n)
+        `X` a nxm matrix
+        `n` number of features
+        `m` no. of dimensions in a feature
+        """
+    
+        ### TRAIN PIPELINE ###
+
+        logging.info('Training model...')
+        self.pipeline.fit(X)
+        logging.info('done.')
+
+        ### Get examplars from each cluster ###
+
+        # Get index (in X) of example closest to cluster center, for each cluster
+        cluster_inds = self.pipeline.predict(X)
+        cluster_space = self.pipeline.transform(X)
+        best_X_inds = []
+        for cluster_ind in range(self.num_examplars):
+            mask = cluster_inds == cluster_ind
+            best_X_ind = np.flatnonzero(mask)[np.argmin(cluster_space[mask, cluster_ind])]
+            best_X_inds.append(best_X_ind)
+
+        return best_X_inds
+
+
+def test():
+    """Test that exemplifier returns index of the closest examplars
+    in a simulated testset"""
+
+    # Toy example
+    n_clusters = 3
+    n_examples_per_cluster = 50
+    stdev = 0.2
+    centers = [(0, 0), (0, 1), (1, 0)]
+    X = np.vstack(
+            np.random.normal(scale=stdev, size=(n_examples_per_cluster, 2)) + center
+            for center in centers)
+
+    exemplifier = FrameExemplifier(n_clusters)
+    exemplar_inds = exemplifier.get_from_features(X)
+
+    plt.figure()
+    fig_title = "X at %s" % time.asctime(time.localtime())
+    plt.gcf().canvas.set_window_title(fig_title)
+    predictions = exemplifier.pipeline.predict(X)
+    for cluster_ind, (color, exemplar_ind) in enumerate(zip('rgb', exemplar_inds)):
+        mask = predictions == cluster_ind
+        mask[exemplar_ind] = False  # don't display the exemplar
+        plt.plot(X[mask, 0], X[mask, 1], color+'o', ms=7)
+        plt.plot(X[exemplar_ind, 0], X[exemplar_ind, 1], color+'o', ms=9,
+                mew=2, label=color+' examplar')
+
+    plt.axis('equal')
+    plt.title('Examples and their predictions')
+    plt.legend(loc='best')
+    plt.show()
+
 
 def main():
 
@@ -32,9 +111,10 @@ def main():
         raise ValueError('Invalid log level: %s' % loglevel)
     logging.basicConfig(level=numeric_level, format='%(asctime)s %(message)s')
 
+    exemplifier = FrameExemplifier(3)
 
     # Load data
-    start, stop, step = 0, 2700, 1
+    start, stop, step = 0, 700, 1
     #start, stop, step = None, None, None
     logging.info('Loading from %s (start frame=%s, end frame=%s, increment=%s)',
         args.input_filename, *map(str, [start, stop, step]))
@@ -42,49 +122,15 @@ def main():
             (itertools.islice(util.grab_frame(args.input_filename), start, stop, step))
     im_HSVs = (cv2.cvtColor(im_original, cv2.COLOR_BGR2HSV)
             for im_original in im_originals)
-    Hhistograms = (np.histogram(im[:,:,0], range=(0, 180.))[0] for im in im_HSVs)
+    Hhistograms = (np.histogram(im[:,:,0], bins=20, range=(0, 180.))[0] for im in im_HSVs)
     X = np.vstack(Hhistograms)
     logging.info('Loaded histograms')
 
-
-    ### DEFINE PIPELINE ###
-
-    n_clusters = 25
-    kmeans = (sklearn.cluster.KMeans,
-            {
-                'n_clusters': n_clusters,
-                #'n_jobs': -1,
-                'n_init': 10,
-                'max_iter': 10,
-            })
-
-    steps = [kmeans]
-    pipeline = sklearn.pipeline.make_pipeline(
-            *[fun(**kwargs) for fun, kwargs in steps])
-
-    util.print_steps(steps)
-
-    
-    ### TRAIN PIPELINE ###
-
-    logging.info('Training model...')
-    pipeline.fit(X)
-    logging.info('done.')
-
-
-    ### Get examplars from each cluster ###
-
-    # Get index (in X) of example closest to cluster center, for each cluster
-    cluster_inds = pipeline.predict(X)
-    cluster_space = pipeline.transform(X)
-    best_X_inds = []
-    for cluster_ind in range(n_clusters):
-        mask = cluster_inds == cluster_ind
-        best_X_ind = np.flatnonzero(mask)[np.argmin(cluster_space[mask, cluster_ind])]
-        best_X_inds.append(best_X_ind)
+    best_X_inds = exemplifier.get_from_features(X)
 
     # Map index in X to index in the input video
     frame_inds = np.arange(start, stop, step)[best_X_inds]
+    #frame_inds = np.linspace(0, 2700, 25, dtype=int)
     frame_inds.sort()
     last_frame_ind = np.asscalar(frame_inds[-1])  # islice below doesn't take np.int64
     im_examplars =  \
@@ -139,5 +185,6 @@ def main():
 
 if __name__ == '__main__':
 
-    main()
+    test()
+    #main()
 
